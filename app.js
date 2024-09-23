47893285ed5466aadde4489de4faf96571948eeb
sameersambhare <sameersambhare@gmail.com>
2024-09-24 01:15:04 +0530

    Production init

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const path=require('path')
 app.use(express.json());
 app.use(cors());
 app.use(require("./routes/authentication"));
+app.use(express.static(path.join(__dirname,"/frontend/build")))
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"/frontend/build/index.html"))
+})
 app.listen(port, () => {
   console.log(`Server starting on port ${port}`);
 });
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,3 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.status(200).json("Router check");
-});
 module.exports = router;
