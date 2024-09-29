134c92539b1299802681aaed0b4d4b0b42ccdf1f
Shashank Dudhane <148575108+ShashankDudhane@users.noreply.github.com>
2024-09-29 15:36:15 +0530

    Update Leaderboard.js

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import leaderboardData from '../data/leaderboardData';
+import leaderboardData from './leaderboardData';
 import './Leaderboard.css';
 
 const Leaderboard = () => {
