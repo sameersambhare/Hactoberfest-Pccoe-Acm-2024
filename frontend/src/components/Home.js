4a3b56453f15bc6d4f7fb82cc3014ab9c4edc71a
Amey Jadhav <ameyjadhav5704@gmail.com>
2024-09-29 16:42:02 +0530

    in home page backround added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Footer from './Footer'
 import Landing from './Landing'
 import Landing2 from './Landing2'
-
+import StarsBackground from '../components/ui/star' 
+import ShootingStars from '../components/ui/shootStar'
 const Home = () => {
   return (
     <div className='w-full min-h-screen relative'>
+      
       <Landing/>
+      <StarsBackground />
+      <ShootingStars />
       {/* <Landing2/> */}
       <Footer/>
     </div>
