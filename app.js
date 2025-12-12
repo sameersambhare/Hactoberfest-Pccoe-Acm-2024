38564b22a2c4c65f35cb9e667e4e85788cafd25e
Sameer Sambhare <sameersambhare@gmail.com>
2025-12-12 17:29:10 +0530

    update react

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(require("./routes/authentication"));
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("", (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 app.listen(port, () => {
